a9b332acf8cd71e8c0fbe2248f00bce60f3c5836
rohhitrz <rohhit.rz@gmail.com>
2026-08-05 20:23:29 +0530

    authentication with better-auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 dotenv.config()
 
 const app=express()
 const PORT=process.env.PORT;
 
+app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use(express.json())
 
 app.get("/",(req,res)=>{
